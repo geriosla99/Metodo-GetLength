b629af27ba13935b5f13ad372b8b7171a8cf2bf4
geriosla99 <geriosla@gmail.com>
2022-08-08 19:03:07 -0500

    Agregar archivos de proyecto.

---
+++ Metodo GetLength/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Metodo_GetLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables 
+            int i;
+            int j;
+
+            double[,] ventas2D = new double[4, 3] { { 100, 120, 205},
+                                                    { 115, 196, 300},
+                                                    { 130, 180, 281},
+                                                    { 157, 172, 245}};
+
+            //Mostramos los valores
+
+            //Console.WriteLine(ventas2D.GetLength(1));
+
+            for (i = 0; i < ventas2D.GetLength(0); i++)
+            {
+                Console.WriteLine("fila {0}", i);
+                for (j = 0; j < ventas2D.GetLength(1); j++)
+                {
+                    Console.WriteLine(ventas2D[i, j]);
+                }
+            }
+        }
+    }
+}
